128399f51ff2b74b9d3b13a846f5b8115b8883c5
Raghav Surya <raghav.surya@outlook.com>
2024-12-13 19:30:01 +0000

    Initial Commit

---
+++ jooz/joozApp.swift
@@ -0,0 +1,17 @@
+//
+//  joozApp.swift
+//  jooz
+//
+//  Created by Raghavendran Suryanarayanan on 13/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct joozApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ joozTests/joozTests.swift
@@ -0,0 +1,16 @@
+//
+//  joozTests.swift
+//  joozTests
+//
+//  Created by Raghavendran Suryanarayanan on 13/12/2024.
+//
+
+import Testing
+
+struct joozTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
